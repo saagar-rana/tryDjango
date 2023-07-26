ddc5e7a569ece1c38526cf9235a91aa32a479bb5
sagar <sagarranabhat757@gmail.com>
2023-07-26 11:35:14 +0545

    Template

---
+++ go/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def setResponse(request):
+    template = loader.get_template('first.html')
+    return HttpResponse(template.render())
+
+# Create your views here.
