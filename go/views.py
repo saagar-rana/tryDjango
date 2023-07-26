06c2f46444402d90c43e5dcd3f65efb30657fb34
sagar <sagarranabhat757@gmail.com>
2023-07-26 17:29:46 +0545

    dynamic template

--- go/views.py
+++ go/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Person
 
 def setResponse(request):
-    template = loader.get_template('first.html')
-    return HttpResponse(template.render())
+    persons = Person.objects.all().values()
+    template = loader.get_template('persons.html')
+    context ={
+        'persons':persons
+    }
+    return HttpResponse(template.render(context,request))
 
 # Create your views here.
